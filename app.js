f6c1b18614514e4a1199381e92f76ed739729261
Anson <leungans@gmail.com>
2018-01-24 19:12:29 -0800

    implement twilio SMS functionality

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Twilio Credentials
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const testRecipient = process.env.TEST_RECEIVER
+const testSender = process.env.TEST_SENDER
+
 // Server
 var express = require('express');
 var app = express();
@@ -13,33 +19,24 @@ serv.listen(3000, function() {
 });
 
 var io = require('socket.io')(serv, {});
-const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID
-const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
 
 io.sockets.on('connection', function(socket) {
-  console.log('Connection made:', socket.id)
+  console.log('Connection from socket id:', socket.id)
 
   socket.on('sendText', function(data) {
-    console.log(TWILIO_ACCOUNT_SID)
-    console.log(TWILIO_AUTH_TOKEN)
 
 
-    // // Twilio Credentials
-    // const accountSid = TWILIO_ACCOUNT_SID;
-    // const authToken = TWILIO_AUTH_TOKEN;
-    //
-    // // require the Twilio module and create a REST client
-    // const client = require('twilio')(accountSid, authToken);
-    //
-    // client.messages
-    //   .create({
-    //     to: '+15558675310',
-    //     from: '+15017122661',
-    //     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-    //   })
-    //   .then(message => console.log(message.sid));
 
+    // require the Twilio module and create a REST client
+    const client = require('twilio')(accountSid, authToken);
 
+    client.messages
+      .create({
+        to: '+' + testRecipient,
+        from: '+' + testSender,
+        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+      })
+      .then(message => console.log(message.sid));
 
 
 
