5cfb60c029e29a842a99cbb5d008266ff2edcff2
Anson <leungans@gmail.com>
2018-01-24 19:00:16 -0800

    setup env vars

--- app.js
+++ app.js
@@ -13,12 +13,36 @@ serv.listen(3000, function() {
 });
 
 var io = require('socket.io')(serv, {});
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
 
 io.sockets.on('connection', function(socket) {
   console.log('Connection made:', socket.id)
 
   socket.on('sendText', function(data) {
-    console.log(data)
+    console.log(TWILIO_ACCOUNT_SID)
+    console.log(TWILIO_AUTH_TOKEN)
+
+
+    // // Twilio Credentials
+    // const accountSid = TWILIO_ACCOUNT_SID;
+    // const authToken = TWILIO_AUTH_TOKEN;
+    //
+    // // require the Twilio module and create a REST client
+    // const client = require('twilio')(accountSid, authToken);
+    //
+    // client.messages
+    //   .create({
+    //     to: '+15558675310',
+    //     from: '+15017122661',
+    //     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+    //   })
+    //   .then(message => console.log(message.sid));
+
+
+
+
+
   })
 
   socket.on('disconnect', function() {
